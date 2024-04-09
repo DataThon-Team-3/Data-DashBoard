96f733670172a2a5f80abeda8790ffa72b203881
kkuhaeda <ansrjsgh316@gmail.com>
2024-04-10 01:59:46 +0900

    이지스토어 위치제안 코드

--- pages/4_gunho.py
+++ pages/4_gunho.py
@@ -4,7 +4,7 @@ import pandas as pd
 import geopandas as gpd
 from gunho_data import india_gpd_table, create_choropleth_mapbox
 
-st.write("# Welcome to Streamlit! 👋")
+st.title("Amazon Easystore 위치 제안")
 
 st.sidebar.success("Select a demo above.")
 
@@ -17,8 +17,32 @@ st.markdown(
 """
 )
 
-# 주 별 매출 금액 시각화
-fig = create_choropleth_mapbox('Amount', '주 별 매출 금액')
-st.plotly_chart(fig)
+col1, col2 = st.columns(2)
 
-st.dataframe(india_gpd_table)
+with col1:
+    plan = st.selectbox('plan',
+        ('주 별 매출금액','주 별 인구대비 매출금액', '주 별 인구대비 시골인구수')
+    )
+    
+if plan == '주 별 매출금액':
+    # 주 별 매출금액 시각화
+    fig = create_choropleth_mapbox('Amount', '매출금액')
+    st.plotly_chart(fig)
+    st.dataframe(india_gpd_table.sort_values(by='Amount', ascending=True))
+    state_name = india_gpd_table.sort_values(by='Amount', ascending=True)
+elif plan == '주 별 인구대비 매출금액':
+    # 주 별 인구대비 매출금액 시각화
+    fig = create_choropleth_mapbox('Amount_population', '매출%')
+    st.plotly_chart(fig)
+    st.dataframe(india_gpd_table.sort_values(by='Amount_population', ascending=True))
+    state_name = india_gpd_table.sort_values(by='Amount_population', ascending=True)
+else:
+    # 주 별 인구대비 시골인구수
+    fig = create_choropleth_mapbox('Rural_ratio', '시골인구%')
+    st.plotly_chart(fig)
+    st.dataframe(india_gpd_table.sort_values(by='Rural_ratio', ascending=False))
+    state_name = india_gpd_table.sort_values(by='Rural_ratio', ascending=False)
+    
+with col2:
+    st.metric(label="추천 위치", value=state_name.index[0])
+    
--- pages/gunho_data.py
+++ pages/gunho_data.py
@@ -18,10 +18,10 @@ india_gpd['Rural_ratio'] = india_gpd['Rural'] / india_gpd['Population'] * 100
 # 주별 인구대비 도시인구
 india_gpd['Urban_ratio'] = india_gpd['Urban'] / india_gpd['Population'] * 100
 
-india_gpd_table = india_gpd[['Qty', 'Amount', 'Population', 'Male', 'Female', 'Rural', 'Urban']].sort_values(by='Amount', ascending=False)
+india_gpd_table = india_gpd[['Qty', 'Amount', 'Population', 'Male', 'Female', 'Rural', 'Urban','Amount_population', 'Rural_ratio']]
 
 
-def create_choropleth_mapbox(color_col, title):
+def create_choropleth_mapbox(color_col, label_text):
     fig = px.choropleth_mapbox(data_frame=india_gpd,
                                geojson=india_gpd.geometry,
                                locations=india_gpd.index,
@@ -32,19 +32,19 @@ def create_choropleth_mapbox(color_col, title):
                                zoom=3,
                                center={"lat": india_gpd['lat'].mean(), "lon": india_gpd['lon'].mean()},
                                opacity=0.5,
-                               labels={color_col:'매출 금액'}
+                               labels={color_col: label_text}
                               )
 
     fig.update_layout(
         margin={"r":0,"t":0,"l":0,"b":0},
-        title={
-            'text': title,
-            'y': 0.95,
-            'x': 0.5,
-            'xanchor': 'center',
-            'yanchor': 'top',
-            'font': {'size': 24}
-        }
+        # title={
+        #     'text': title,
+        #     'y': 0.95,
+        #     'x': 0.5,
+        #     'xanchor': 'center',
+        #     'yanchor': 'top',
+        #     'font': {'size': 24}
+        # }
     )
 
     return fig
