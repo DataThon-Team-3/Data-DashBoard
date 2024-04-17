96f603222cc6f991b7b4ad138ac28fa91199ef23
kkuhaeda <ansrjsgh316@gmail.com>
2024-04-17 10:18:01 +0900

    수정완료

--- pages/4_gunho.py
+++ pages/4_gunho.py
@@ -2,7 +2,65 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import geopandas as gpd
-from gunho_data import india_gpd_table, create_choropleth_mapbox
+
+# st.set_page_config(layout='wide')
+# st.markdown(
+#     """
+# <style>
+# [data-testid="stAppViewBlockContainer"] {
+#     padding: 1rem 30rem 5rem 30rem;
+# }
+# </style>
+# """,
+#     unsafe_allow_html=True,
+# )
+
+india_gpd = gpd.read_file('./data/gunho/india_gpd/India_gpd.shp')
+
+india_gpd = india_gpd.drop(1)
+india_gpd = india_gpd.set_index('ship_state')
+
+india_gpd['center'] = india_gpd['geometry'].centroid
+india_gpd['lon'] = india_gpd['center'].x
+india_gpd['lat'] = india_gpd['center'].y
+
+# 주별 인구대비 매출금액
+india_gpd['Amount_population'] = india_gpd['Amount'] / india_gpd['Population'] * 100
+# 주별 인구대비 시골인구
+india_gpd['Rural_ratio'] = india_gpd['Rural'] / india_gpd['Population'] * 100
+# 주별 인구대비 도시인구
+india_gpd['Urban_ratio'] = india_gpd['Urban'] / india_gpd['Population'] * 100
+
+india_gpd_table = india_gpd[['Qty', 'Amount', 'Population', 'Male', 'Female', 'Rural', 'Urban','Amount_population', 'Rural_ratio']]
+
+
+def create_choropleth_mapbox(color_col, label_text, color_style):
+    fig = px.choropleth_mapbox(data_frame=india_gpd,
+                               geojson=india_gpd.geometry,
+                               locations=india_gpd.index,
+                               color=color_col,
+                               color_continuous_scale=color_style,
+                               range_color=(india_gpd[color_col].min(), india_gpd[color_col].max()),
+                               mapbox_style="carto-positron",
+                               zoom=3,
+                               center={"lat": india_gpd['lat'].mean(), "lon": india_gpd['lon'].mean()},
+                               opacity=0.5,
+                               labels={color_col: label_text}
+                              )
+
+    fig.update_layout(
+        margin={"r":0,"t":0,"l":0,"b":0},
+        # title={
+        #     'text': title,
+        #     'y': 0.95,
+        #     'x': 0.5,
+        #     'xanchor': 'center',
+        #     'yanchor': 'top',
+        #     'font': {'size': 24}
+        # }
+    )
+
+    return fig
 
 st.title("Amazon Easystore 위치 제안")
 
